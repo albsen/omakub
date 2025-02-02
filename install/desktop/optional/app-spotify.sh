a60ef8d48f185c9d43e1913d8280f213f3ad5a2d
A. Sebastian Dietzel <albsen@gmail.com>
2025-02-02 21:04:34 +0400

    Squashed:
    Add support for arm64 where available
    Update spotify to check for arm
    Fix signal
    Fix signal
    Fix signal
    
    Author: Adam Brown <avb@dicebag.com>
    https://github.com/avb/omakub/tree/arm-support

--- install/desktop/optional/app-spotify.sh
+++ install/desktop/optional/app-spotify.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Stream music using https://spotify.com
+if [ "$(dpkg --print-architecture)" != "arm64" ]; then
 curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb [signed-by=/etc/apt/trusted.gpg.d/spotify.gpg] https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update -y
 sudo apt install -y spotify-client
+fi
